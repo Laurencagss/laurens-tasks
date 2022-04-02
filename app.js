5be1270e06d5f5ad38f9c97726d1f77c42bbda8b
Lauren Cagnetta <laurenscolleges@gmail.com>
2022-04-02 09:54:28 -0400

    using webpack

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const path = require('path');
 const { User, sequelize, Task } = require('./db');
 
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use ('/api/users', require('./routes/users'));
 app.use ('/api/tasks', require('./routes/tasks'));
