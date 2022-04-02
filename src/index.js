bb9099dd4bd8aa038c710958e43a59449b2596c5
Lauren Cagnetta <laurenscolleges@gmail.com>
2022-04-02 11:03:32 -0400

    added a bunch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import App from './components/App';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const root = document.querySelector('#root');
-ReactDOM.render(<hr />, root);s
+ReactDOM.render(<HashRouter><Provider store= {store}><App /></Provider></HashRouter>, root);
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import {createStore, combineReducers} from 'redux';
+ const tasks = (state = [], action) => {
+     return state;
+    }
+    const users = (state = [], action) => {
+        return state;
+    }
+
+    const reducer = combineReducers({
+        tasks: tasks,
+        users: users
+    });
+
+    const store= createStore(reducer);
+
+    export default store;
